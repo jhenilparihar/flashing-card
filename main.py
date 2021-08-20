16cc61e5d433fc2917bf4f60aa2fb8a392c450ad
Kartikeya <menonkartikeya@gmail.com>
2021-08-20 19:56:13 +0530

    Added disable button changes

--- main.py
+++ main.py
@@ -28,63 +28,72 @@ def word_generate():
         index = random.randint(0, len(data))-1
         word_dict = data[index]
     except IndexError:
-        print('Starting Your Tutorial Again')
+        print("You learnt all these French Words!")
+        print('Starting Your Tutorial Again...')
         data = pandas.read_csv('data/french_words.csv')
         data = data.to_dict(orient='records')
         word_generate()
     return word_dict
 
 
+def flip_card_now():
+    canvas.itemconfig(image, image=front_image)
+    canvas.itemconfig(title, text='French', fill="white")
+    new_french_word = word_generate()['French']
+    canvas.itemconfig(word, text=new_french_word, fill="white")
+
+    right_button["state"] = "disabled"
+    wrong_button["state"] = "disabled"
+    
+    window.after(3000, flip_card)
+    
 def right():
     data.pop(index)
     update()
-
-    canvas.itemconfig(image, image=front_image)
-    canvas.itemconfig(title, text='French', fill="black")
-    new_french_word = word_generate()['French']
-    canvas.itemconfig(word, text=new_french_word, fill="black")
-
-    window.after(3000, flip_card)
-
+    flip_card_now()
 
 def wrong():
-    canvas.itemconfig(image, image=front_image)
-    canvas.itemconfig(title, text='French', fill="black")
-    new_french_word = word_generate()['French']
-    canvas.itemconfig(word, text=new_french_word, fill="black")
-
-    window.after(3000, flip_card)
+    flip_card_now()
 
 
 def flip_card():
     canvas.itemconfig(image, image=back_image)
-    canvas.itemconfig(title, text="English", fill="white")
-    canvas.itemconfig(word, text=word_dict["English"], fill="white")
+    canvas.itemconfig(title, text="English", fill="black")
+    canvas.itemconfig(word, text=word_dict["English"], fill="black")
+    
+    right_button["state"] = "normal"
+    wrong_button["state"] = "normal"
 
 
 window = Tk()
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+try:
+    window.attributes('-zoomed', True)
+except:
+    window.state('zoomed')
 window.title('Flashing Card')
 
 canvas = Canvas(height=526, width=800, highlightthickness=0, bg=BACKGROUND_COLOR)
-front_image = PhotoImage(file='images/card_front.png')
-back_image = PhotoImage(file='images/card_back.png')
+front_image = PhotoImage(file='images/card_back.png')
+back_image = PhotoImage(file='images/card_front.png')
 
 image = canvas.create_image(400, 263, image=front_image)
 
 title = canvas.create_text(400, 150, text='French', font=('Arial', 30, 'italic'))
 word = canvas.create_text(400, 263, text=word_generate()['French'], font=('Arial', 60, 'bold'))
+    
+canvas.place(relx = 0.5, rely=0.01, anchor=N)
 
-window.after(3000, flip_card)
-
-canvas.grid(column=0, row=0, columnspan=2)
 
 wrong_image = PhotoImage(file='images/wrong.png')
 wrong_button = Button(image=wrong_image, height=99, width=100, highlightthickness=0, border=0, command=wrong)
-wrong_button.grid(column=0, row=1)
+wrong_button.place(relx = 0.25, rely=0.88, anchor=N)
 
 right_image = PhotoImage(file='images/right.png')
 right_button = Button(image=right_image, height=99, width=100, highlightthickness=0, border=0, command=right)
-right_button.grid(column=1, row=1)
+right_button.place(relx = 0.75, rely=0.88, anchor=N)
+
+
+flip_card_now()
 
 window.mainloop()
